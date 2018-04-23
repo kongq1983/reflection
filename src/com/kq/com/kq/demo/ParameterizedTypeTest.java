aece931ea731d5835f192adc4264ceeb4e973131
king <kongq1983@qq.com>
2018-04-24 00:27:37 +0800

    add ParameterizedTypeTest

---
+++ src/com/kq/com/kq/demo/ParameterizedTypeTest.java
@@ -0,0 +1,39 @@
+package com.kq.com.kq.demo;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class ParameterizedTypeTest {
+
+    public String printList(List<String> strLst) {
+        return "";
+    }
+
+    public static void parameterizedTypeDemo(){
+        Method[] ms  = ParameterizedTypeTest.class.getDeclaredMethods();
+
+        for(Method m : ms) {
+            if(m.getName().equals("printList")) {
+                System.out.println("printList method : "+m);
+                Type[] types = m.getGenericParameterTypes();
+
+                for(Type t : types) {
+                    System.out.println(t.getTypeName());
+                    System.out.println(t.getClass());
+                    if(t instanceof ParameterizedType) {
+                        System.out.println("equal");
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        parameterizedTypeDemo();
+    }
+
+}
